ac50532d519af4ec1805a410915b6ac6acff80b4
Jack Franklin <jack@jackfranklin.net>
2014-07-11 17:24:22 +0100

    update all the dependencies

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'jade');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
