240e40c1fc730e96c68dd4b4eafe7076ee04dd91
Jack Franklin <jack@jackfranklin.net>
2014-07-11 17:25:44 +0100

    remove favicon

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
