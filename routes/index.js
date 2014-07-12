3e8e12a1d0649419df96cdf7d40f0e687c8cdac7
Jack Franklin <jack@jackfranklin.net>
2014-07-12 21:40:48 +0100

    allow access from any url

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,11 @@
 module.exports = function(app) {
+
+  app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+
   app.get('/', function(req, res){
     res.render('index', { title: 'Express' });
   });
